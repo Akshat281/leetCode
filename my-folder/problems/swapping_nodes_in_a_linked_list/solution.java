becdc59778ea5d13c7675f0805336631682e2672
AKSHAT ANAND <95584233+Akshat281@users.noreply.github.com>
2023-05-15 16:56:39 +0000

    Sync LeetCode submission - Swapping Nodes in a Linked List (java)

---
+++ my-folder/problems/swapping_nodes_in_a_linked_list/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        // find the first node to be swapped
+        ListNode node1 = head;
+        for (int i = 1; i < k; i++) {
+            node1 = node1.next;
+        }
+        
+        // find the second node to be swapped
+        ListNode node2 = head;
+        ListNode current = node1;
+        while (current.next != null) {
+            current = current.next;
+            node2 = node2.next;
+        }
+        
+        // swap the nodes by changing the pointers
+        int temp = node1.val;
+        node1.val = node2.val;
+        node2.val = temp;
+        
+        return head;
+    }
+}
